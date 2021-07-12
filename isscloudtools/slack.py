95c81e9b6efb506cdb5e97059e79dc395e589c70
dleshchev <leshchev.denis@gmail.com>
2021-07-12 09:51:45 -0400

    added bnl denis to users

--- isscloudtools/slack.py
+++ isscloudtools/slack.py
@@ -3,7 +3,7 @@ def slack_create_channel(client,channel_name ):
     return channel.data['channel']['id'], channel.data['channel']
 
 
-def slack_invite_to_channel(client, channel_id, users = ['U016TSW4MSB', 'U018F0DLENQ']):
+def slack_invite_to_channel(client, channel_id, users = ['U016TSW4MSB', 'U018F0DLENQ', 'U01RR69G6E8']):
     return client.conversations_invite(channel=channel_id, users=users).data['ok']
 
 
